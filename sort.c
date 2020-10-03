a94994a4c544ddb813f226c50837b73e13b68f20
hfu11 <61071935+hfu11@users.noreply.github.com>
2020-10-03 00:00:37 -0400

    Update sort.c

--- sort.c
+++ sort.c
@@ -17,15 +17,21 @@
 
 #define MAX_COUNT 1000000 // Max integers to sort
 #define MAX_NUM 100 // Generate numbers between 0 and MAX_NUM
-#define MAX_P 100
+#define MAX_P 10
 
-// Uncomment the following line to turn on debugging messages
-// #define DEBUG
-#define Timer
+// Uncomment the following line to turn on printing messages
+#define PRINT
+// #define Timer
 
 // long number[MAX_COUNT];
 int N; // Number of integers to sort
 int P;
+
+long* number;
+int* pass_exit;
+int* doneArr;
+int* countArr;
+
 
 // generate N random numbers between 0 and MAX_NUM
 void generate_numbers(long * number)
@@ -47,7 +53,7 @@ void print_numbers(long * number)
 	printf("\n");
 }
 
-int compare_and_swap(int i, int j,long * number) 
+int compare_and_swap(int i, int j) 
 {
 #ifdef DEBUG
 	fprintf(stderr,"i %d j %d\n", i, j);
@@ -66,57 +72,36 @@ int compare_and_swap(int i, int j,long * number)
 }
 
 // even-odd pass bubbling from start to start+n
-int bubble(int start, int n, int pass,long * number) 
+int bubble(int start, int n, int pass) 
 {
 #ifdef DEBUG
 	fprintf(stderr, "start %d n %d pass %d\n", start, n, pass);
 #endif
-
+	
 	int swap_count = 0;
 	int next = start;
 
 	assert (start < N-1); // bug if we start at the end of array
 
-	if (pass) { // sort odd-even index pairs
-		if ( !(next % 2) ) 
+	if (pass == 1) { // sort odd-even index pairs
+		if ( next%2 == 0 ) 
 			next = next + 1;
 	} else  { // sort even-odd index pairs
-		if (next % 2) 
+		if (next%2 != 0) 
 			next = next + 1;
 	}
 
 	while ( (next+1) < (start+n) ) {
-		swap_count += compare_and_swap(next, next+1,number);
+		swap_count += compare_and_swap(next, next+1);
 		next+=2;
 	}
 
 	return swap_count;
 }
 
-void bubble_sort(long * number) 
-{
-	int last_count, swap_count = N;
-	int pass = 0;
-
-#ifdef DEBUG
-	print_numbers();
-#endif
-
-	do {
-		last_count = swap_count;
-		swap_count = bubble(0, N, pass,number); // 0 for single-process sorting
-#ifdef DEBUG
-		print_numbers();
-		fprintf(stderr,"last_count %d swap_count %d\n", last_count, swap_count);
-#endif
-		pass = 1-pass;
-	} while((swap_count!=0) || (last_count != 0));
-}
-
-long* generate_shm(){
+void generate_shm(){
 	key_t key;
 	int shmid;
-	long *data;
 	int mode;
 
 
@@ -125,8 +110,6 @@ long* generate_shm(){
 		perror("ftok");
 		exit(1);
 	}
-
-
 	/* connect to t
 ]=h\e segment. 
 	NOTE: There's no IPC_CREATE. What happens if you place IPC_CREATE here? */
@@ -134,44 +117,118 @@ long* generate_shm(){
 		perror("shmget");
 		exit(1);
 	}
-
-/* attach to the segment to get a pointer to it: */
-	data = (long *)shmat(shmid, (void *)0, 0);
-	if (data == (long *)(-1)) {
+	number = (long *)shmat(shmid, (void *)0, 0);
+	if (number == (long *)(-1)) {
 		perror("shmat");
 		exit(1);
 	}
 
-	return data;
-}
-
-void DoWorkInChild( int index,long * number ){
-	//set up parameters
-	int n = (int)ceil(N/P);
-	int start = index * (n-1);
-	if( index == P - 1 && start + n < N){
-		n += 1;
+	if ((key = ftok("test_shm", 'C')) == -1) {
+		perror("ftok");
+		exit(1);
+	}
+	if ((shmid = shmget(key, 10*4 , IPC_CREAT | 0644)) == -1) {
+		perror("shmget");
+		exit(1);
+	}
+	pass_exit = (int *)shmat(shmid, (void *)0, 0);
+	if (pass_exit == (int*)-1) {
+		perror("shmat");
+		exit(1);
 	}
 
-	//exec sort
-	int last_count, swap_count = N;
-	int pass = 0;
+	if ((key = ftok("test_shm", 'D')) == -1) {
+		perror("ftok");
+		exit(1);
+	}
+	if ((shmid = shmget(key, MAX_P*4 , IPC_CREAT | 0644)) == -1) {
+		perror("shmget");
+		exit(1);
+	}
+	doneArr = (int *)shmat(shmid, (void *)0, 0);
+	if (doneArr == (int*)-1) {
+		perror("shmat");
+		exit(1);
+	}
 
-#ifdef DEBUG
-	print_numbers();
-#endif
-
-	do {
-		last_count = swap_count;
-		swap_count = bubble(0, N, pass,number); // 0 for single-process sorting
-#ifdef DEBUG
-		print_numbers();
-		fprintf(stderr,"last_count %d swap_count %d\n", last_count, swap_count);
-#endif
-		pass = 1-pass;
-	} while((swap_count!=0) || (last_count != 0));
-
+	if ((key = ftok("test_shm", 'E')) == -1) {
+		perror("ftok");
+		exit(1);
+	}
+	if ((shmid = shmget(key, MAX_P*4 , IPC_CREAT | 0644)) == -1) {
+		perror("shmget");
+		exit(1);
+	}
+	countArr = (int *)shmat(shmid, (void *)0, 0);
+	if (countArr == (int*)-1) {
+		perror("shmat");
+		exit(1);
+	}
 }
+
+
+
+void DoWorkInChild(int index){
+	//set up parameters
+	int n = N/P+1;
+	
+	int start = index * (n-1);
+	if( index == P - 1){
+		n = N - start;
+	}
+
+	fprintf(stderr, "start %d n %d\n", start, n);
+
+	// printf("child %d here\n",index);
+
+	//barrier
+	while(1){
+		if(pass_exit[0] == 1) while(pass_exit[0] == 1);
+		countArr[index] = bubble(start, n, 0); // even
+		doneArr[index] = 1;
+		if(pass_exit[1] == 1) exit(0);
+
+		if(pass_exit[0] == 0) while(pass_exit[0] == 0);
+		countArr[index] = bubble(start, n, 1); // odd
+		doneArr[index] = 1;
+		if(pass_exit[1] == 1) exit(0);
+	}
+
+
+	exit(0);
+}
+
+void initDoneArr(){
+	int i;
+	for(i=0;i<P;i++){
+		doneArr[i] = 0;
+	}
+}
+
+void initCountArr(){
+	int i;
+	for(i=0;i<P;i++){
+		countArr[i] = 0;
+	}
+}
+
+int checkDone(){
+	int i;
+	for(i=0;i<P;i++){
+		if(doneArr[i] == 0) return 0;
+	}
+	return 1;
+}
+
+int checkCount(){
+	int i;
+	int count = 0;
+	for(i=0;i<P;i++){
+		count += countArr[i];
+	}
+	return count;
+}
+
 
 int CheckSort(long * number){
 	int i;
@@ -209,14 +266,23 @@ int main(int argc, char *argv[])
 	struct timeval start,end;
 	gettimeofday(&start,NULL);
 	#endif
+#pragma region number, pass_exit, doneArr, countArr
+	generate_shm();
+	pass_exit[0] = 0;
+	pass_exit[1] = 0;
+
+	initDoneArr();
+	initCountArr();
 
 	//fprintf(stderr, "Generating.\n");
-	long* number = generate_shm();
 
 	generate_numbers(number);
+#pragma endregion
 
-	// fprintf(stdout, "Generated sequence is as follows:\n");
-	// print_numbers(number);
+#ifdef PRINT
+	fprintf(stdout, "Generated sequence is as follows:\n");
+	print_numbers(number);
+#endif
 
 	pid_t pids[P];
 	int i;
@@ -229,39 +295,67 @@ int main(int argc, char *argv[])
 			abort();
 		} 
 		else if (pids[i] == 0) {
-			DoWorkInChild(i,number);
-			exit(0);
+			DoWorkInChild(i);
+			// exit(0);
 		}
 	}
 
 	// Wait for children to exit.
-	int status;
-	pid_t pid;
-	while (n > 0) {
-	pid = wait(&status);
-	// printf("Child with PID %ld exited with status 0x%x.\n", (long)pid, status);
-	n--;
-	}
-	// Parent Work
-	// if(getpid() > 0){
-	// 	DoWorkInParent(number);
+	// int status;
+	// pid_t pid;
+	// while (n > 0) {
+	// pid = wait(&status);
+	// // printf("Child with PID %ld exited with status 0x%x.\n", (long)pid, status);
+	// n--;
 	// }
 
-	//fprintf(stderr, "Sorting.\n");
-	// bubble_sort(number);
+	//parent work
+	int itr;
+	int last_count,count = N;
+	
+	do{
+		last_count = count;
+		// printf("\ndoneArr:\n");
+		// for(itr=0;itr<P;itr++){
+		// 	printf("%d ",doneArr[itr]);
+		// }
+		// printf("\n");
 
-	fprintf(stdout, "Check Sort:\n");
+		while(checkDone() != 1){
+			printf("\ndoneArr:\n");
+			for(itr=0;itr<P;itr++){
+				printf("%d ",doneArr[itr]);
+			}
+			printf("\n");
+		}
+
+		
+		count = checkCount();
+
+		// print_numbers(number);
+
+		// for(itr=0;itr<P;itr++){
+		// 	printf("%d ",countArr[itr]);
+		// }
+		// printf("\n");
+
+		pass_exit[0] = 1 - pass_exit[0];
+		initDoneArr(); //clear done array
+	}while( (count != 0) || (last_count != 0));
+	pass_exit[1] = 1;
+
+	// fprintf(stdout, "Check Sort:\n");
 	if(CheckSort(number) == 0){
 		fprintf(stdout, "Sort Failed\n");
 	}
 	else{
 		fprintf(stdout, "Sort Succeeded\n");
 	}
-
-	// fprintf(stdout, "Sorted sequence is as follows:\n");
-	// print_numbers(number);
+#ifdef PRINT
+	fprintf(stdout, "Sorted sequence is as follows:\n");
+	print_numbers(number);
 	// fprintf(stderr, "Done.\n");
-
+#endif
 	
 
 	#ifdef Timer
